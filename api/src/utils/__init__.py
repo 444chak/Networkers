4e447576f5b14f9ce6c2470e99b91649cf68000e
Eliott-B <eliottb.info@gmail.com>
2024-12-16 10:46:51 +0100

    Add auth module

---
+++ api/src/utils/__init__.py
@@ -0,0 +1,7 @@
+"""Package for utils."""
+from .auth import (
+    create_access_token,
+    create_refresh_token,
+    get_hashed_password,
+    verify_password,
+)
---
+++ api/src/utils/auth.py
@@ -0,0 +1,81 @@
+"""Module for authentication utilities."""
+import os
+from datetime import UTC, datetime, timedelta
+
+from jose import jwt
+from passlib.context import CryptContext
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
+ALGORITHM = "HS256"
+JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+JWT_REFRESH_SECRET_KEY = os.environ["JWT_REFRESH_SECRET_KEY"]
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_hashed_password(password: str) -> str:
+    """Return the hashed password.
+
+    Args:
+        password (str): password to hash
+
+    Returns:
+        str: Hashed password
+
+    """
+    return password_context.hash(password)
+
+
+def verify_password(password: str, hashed_pass: str) -> bool:
+    """Return True if the password is correct, False otherwise.
+
+    Args:
+        password (str): password to verify
+        hashed_pass (str): hashed password reference
+
+    Returns:
+        bool: true if the password is correct, false otherwise
+
+    """
+    return password_context.verify(password, hashed_pass)
+
+
+def create_access_token(subject: str, expires_delta: int | None = None) -> str:
+    """Return the access token.
+
+    Args:
+        subject (str): User to authenticate
+        expires_delta (int | None, optional): Expire delta in minutes. Defaults to None.
+
+    Returns:
+        str: Access token
+
+    """
+    if expires_delta is not None:
+        expires_delta = datetime.now(tz=UTC) + expires_delta
+    else:
+        expires_delta = datetime.now(tz=UTC) + \
+            timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    return jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+
+def create_refresh_token(subject: str, expires_delta: int | None = None) -> str:
+    """Return the refresh token.
+
+    Args:
+        subject (str): User to authenticate
+        expires_delta (int | None, optional): Expire delta in minutes. Defaults to None.
+
+    Returns:
+        str: Refresh token
+
+    """
+    if expires_delta is not None:
+        expires_delta = datetime.now(tz=UTC) + expires_delta
+    else:
+        expires_delta = datetime.now(tz=UTC) + \
+            timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    return jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
