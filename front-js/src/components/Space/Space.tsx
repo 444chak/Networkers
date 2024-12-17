9e1b1b631be1453f638dd70457604372f44b4636
444chak <444chak@gmail.com>
2024-12-17 19:33:45 +0100

    ✨feature: Amélioration du composant Space avec des tailles personnalisables et ajustement de l'affichage sur la page d'accueil

--- front-js/src/components/Space/Space.tsx
+++ front-js/src/components/Space/Space.tsx
@@ -3,14 +3,15 @@
 */
 'use client';
 
-
 import React from 'react';
 import './Space.scss';
 
 interface SpaceProps {
     children?: React.ReactNode;
     direction?: 'horizontal' | 'vertical';
+    space?: string;
     spaceBetween?: boolean;
+    sizes?: string[];
     margin?: {
         left?: string;
         right?: string;
@@ -19,13 +20,18 @@ interface SpaceProps {
     }
 }
 
-const Space: React.FC<SpaceProps> = ({ children, spaceBetween, direction, margin }) => {
+const Space: React.FC<SpaceProps> = ({ children, space, direction, margin, sizes, spaceBetween }) => {
+    const isHorizontal = direction === 'horizontal';
+    const marginStyle = isHorizontal ? { marginRight: space } : { marginBottom: space };
+
     return (
         <div className={`space ${direction} ${spaceBetween ? 'space-between' : ''}`} style={{ marginLeft: margin?.left, marginRight: margin?.right, marginTop: margin?.top, marginBottom: margin?.bottom }}>
-            {children}
+            {React.Children.map(children, (child, index) => (
+                <div style={{ flexBasis: sizes ? sizes[index] : 'auto', ...((index < React.Children.count(children) - 1) ? marginStyle : {}) }}>
+                    {child}
+                </div>
+            ))}
         </div >
-
-
     );
 };
 
