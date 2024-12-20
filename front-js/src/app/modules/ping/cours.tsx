590cca6e7889d2b7af790e26584a22a1854ec4e1
MaxOuvrard <118462372+MaxOuvrard@users.noreply.github.com>
2024-12-20 10:47:53 +0100

    Add lesson for ping card

--- front-js/src/app/modules/ping/cours.tsx
+++ front-js/src/app/modules/ping/cours.tsx
@@ -23,8 +23,9 @@ const Cours: React.FC = () => {
   const handlePing = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     try {
+      const ipAddress = pingTest.replace('ping ', '');
       const response = await axios.get(
-        "/scapy/ping/127.0.0.1",
+        "/scapy/ping/" + ipAddress,
         {
           headers: {
             Authorization: `Bearer ${Cookies.get("access_token")}`,
@@ -33,14 +34,16 @@ const Cours: React.FC = () => {
       );
       const data = response.data;
       if (response.status === 200) {
-        setRes(data.ipv6);
-        setValid(res == pingTest);
+        setRes(data.destination);
+        setTimeout(() => {
+          setValid(data.destination === "193.51.31.90");
+        }, 0);
       }
     } catch (error: unknown) {
       const axiosError = error as AxiosError;
       if (axiosError.response?.status === 400) {
         const data = axiosError.response.data as { detail: string };
-        if (data.detail === "Invalid IPv6") {
+        if (data.detail === "Invalid ping") {
           setRes("false");
         } else {
           setRes("false");
@@ -57,27 +60,41 @@ const Cours: React.FC = () => {
         <Space space="1rem">
           <Title level={2}>Introduction</Title>
           <Text>
-            Le ping est un outil réseau de base utilisé pour tester la connectivité entre deux appareils 
-            sur un réseau, en envoyant des paquets ICMP Echo Request et en attendant des paquets ICMP.
-            Dans ce cours, nous allons utiliser la librairie Scapy en Python pour simuler l'envoi d'un ping 
-            à travers le réseau. Scapy permet de manipuler facilement les paquets à différents niveaux du 
-            modèle OSI, et dans ce cas, nous allons nous concentrer sur les paquets ICMP au niveau de la 
-            couche réseau (couche 3 du modèle OSI).
+          Le ping est un outil de diagnostic réseau utilisé pour tester la connectivité entre deux hôtes
+           sur un réseau IP. Il fonctionne en envoyant des paquets ICMP (Internet Control Message Protocol)
+            de type à un hôte cible et en attendant des réponses. 
+            Le ping est couramment utilisé pour vérifier si un hôte est accessible, mesurer le temps de
+            latence entre deux hôtes, et diagnostiquer les problèmes de réseau.
           </Text>
           <Title level={2} margin={{ top: "2rem" }}>
             Structure paquet ICMP
           </Title>
           <Text>
-            Un paquet ICMP utilisé pour le ping est composé de plusieurs parties :
+            Un paquet ICMP est encapsulé dans un paquet IP et contient plusieurs champs spécifiques.
+            La structure d'un paquet ICMP Requête/Réponse est la suivante : 
           </Text>
           <Text>
-            1. IP : l'en-tête IP (version 4 ou 6) qui contient des informations sur l'adresse de 
-            destination et l'origine du paquet.
+            L'entête est composée de :
           </Text>
           <Text>
-            2. ICMP : l'en-tête du protocole ICMP qui contient le type, le code et des informations 
-            supplémentaires comme un identifiant et un numéro de séquence.
+            1. Type (1 octet) : Ce champ indique le type de message ICMP.
+          </Text>
+          <Text>
+            2. Code (1 octet) : Fournit des informations supplémentaires sur le type.
         </Text>
+        <Text>
+            3. Checksum (2 octets) : Ce champ contient une somme de contrôle utilisée pour vérifier l'intégrité du paquet ICMP.
+          </Text>
+          <Text>
+            4. dentifiant (2 octets) : Identifie de manière unique une requête/réponse.
+          </Text>
+          <Text>
+            5. Numéro de séquence (2 octets) : Ce champ est utilisé pour numéroter les paquets dans une session de ping,
+             permettant de détecter les pertes de paquets.
+          </Text>
+          <Text>
+            Après l'entête, nous retrouvons toutes les données qui sont envoyés dans le paquet ICMP.
+          </Text>
         <Text>
            Voici un exemple de commande que vous pourriez utiliser afin de réaliser un ping :
             <Spacer x={1} />
@@ -90,12 +107,47 @@ const Cours: React.FC = () => {
           <Title level={2} margin={{ top: "2rem" }}>
             Réponse d'une requête ping
           </Title>
+          <Text>
+          Envoi de l'Echo Request :
+          </Text>
+          <Text>
+          Lorsque vous envoyez un ping à un autre appareil, votre ordinateur crée un petit message appelé "Echo Request".
+          Ce message est envoyé à l'adresse IP de l'appareil cible.
+          </Text>
+          <Text>
+          Réception de l'Echo Request :
+          </Text>
+          <Text>
+          L'appareil cible reçoit le message "Echo Request".
+          Il vérifie que le message n'est pas corrompu en utilisant une somme de contrôle (checksum).
+          </Text>
+          <Text>
+          Création de l'Echo Reply :
+          </Text>
+          <Text>
+          Si le message est correct, l'appareil cible crée un nouveau message appelé "Echo Reply".
+          Ce message contient les mêmes informations que l'Echo Request, mais avec un type différent pour indiquer qu'il s'agit d'une réponse.
+          </Text>
+          <Text>
+          Envoi de l'Echo Reply :
+          </Text>
+          <Text>
+          L'appareil cible envoie le message "Echo Reply" à votre ordinateur.
+          Votre ordinateur reçoit ce message et peut ainsi confirmer que l'appareil cible est accessible.
+          </Text>
+          <Text>
+          Mesure du Temps de Réponse :
+          </Text>
+          <Text>
+          Votre ordinateur mesure le temps écoulé entre l'envoi de l'Echo Request et la réception de l'Echo Reply.
+          Ce temps est appelé "latence" et vous donne une idée de la rapidité de la connexion réseau.
+          </Text>
           <Title level={2} margin={{ top: "2rem" }}>
             Essayez ! <Emoji name="test-tube" width={32} />
           </Title>
-          Tentez de ping le site de Google.
+          Tentez de ping le site de l'iut de Vélizy.
           <Code
-            text="https://google.com"
+            text="https://www.iut-velizy-rambouillet.uvsq.fr/"
             language="text"
             theme={solarizedLight}
           />
