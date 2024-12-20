09c4a40d7ccfc639150ec4341f798ceb4f8bc780
Chak <444chak@gmail.com>
2024-12-20 17:20:37 +0100

    🐞 fix: remplacer la redirection de la fenêtre par l'utilisation de useRouter pour la déconnexion

--- front-js/src/app/auth/logout/page.tsx
+++ front-js/src/app/auth/logout/page.tsx
@@ -4,11 +4,13 @@ import Layout from "@/components/Layout";
 import Box from "@/components/Box";
 import Cookies from "js-cookie";
 import CircularProgress from "@mui/material/CircularProgress";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   Cookies.remove("access_token");
   Cookies.remove("refresh_token");
-  window.location.href = "/";
+  router.push("/");
 
   return (
     <Layout type="home">
