cb7bd43e1065c49a797d25999615f21a6abf9371
MaxOuvrard <118462372+MaxOuvrard@users.noreply.github.com>
2024-12-20 14:58:40 +0100

    Create footer component

---
+++ front-js/src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import "./Footer.scss";
+
+
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <div className="footer-content">
+          <p>© 2024 - SAE 5.01 - Barker Ouvrard Rubio Gravier Ouali</p>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
