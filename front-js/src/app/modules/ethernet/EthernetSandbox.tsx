38a63c3ca10d13adc0368a11163c7ef43ff5a3c2
SaAxok <grv.kylian@gmail.com>
2024-12-20 10:55:07 +0100

    finish ethernet module

--- front-js/src/app/modules/ethernet/EthernetSandbox.tsx
+++ front-js/src/app/modules/ethernet/EthernetSandbox.tsx
@@ -4,136 +4,110 @@ import Input from "@/components/Input";
 import Space from "@/components/Space";
 import React from "react";
 import { useState } from "react";
-import axios from "@/axiosConfig";
-import Cookies from "js-cookie";
-import { AxiosError } from "axios";
 import { Alert } from "@mui/material";
 import Box from "@/components/Box";
+import Cookies from "js-cookie";
+import axios from "@/axiosConfig";
+import { AxiosError } from "axios";
+import { Code, solarizedLight } from "react-code-blocks";
 
 const EthernetSandbox: React.FC = () => {
-  const [simpleIpv6, setSimpleIpv6] = useState("");
-  const [simpleRes, setSimpleRes] = useState("");
+    const [masdst, setMacdst] = useState("");
+    const [macsrc, setMacsrc] = useState("");
+    const [typetrame, setTypetrame] = useState("");
+    const [simpleRes, setSimpleRes] = useState("");
 
-  const handleSimplify = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    try {
-      const response = await axios.get("/ipv6/simplify/ " + simpleIpv6, {
-        headers: {
-          Authorization: `Bearer ${Cookies.get("access_token")}`,
-        },
-      });
-      const data = response.data;
-      if (response.status === 200) {
-        setSimpleRes(data.ipv6);
-      }
-    } catch (error: unknown) {
-      const axiosError = error as AxiosError;
-      if (axiosError.response?.status === 400) {
-        const data = axiosError.response.data as { detail: string };
-        if (data.detail === "Invalid IPv6") {
-          setSimpleRes("Adresse IPv6 invalide");
-        } else {
-          setSimpleRes("Adresse IPv6 invalide");
+    const handleEthernet = async (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        try {
+            const response = await axios.get(
+                `/scapy/ethernet-frame/${masdst}/${macsrc}/${typetrame}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${Cookies.get("access_token")}`,
+                    },
+                }
+            );
+            const data = response.data;
+            if (response.status === 200) {
+                setSimpleRes(data.frame);
+            }
+        } catch (error: unknown) {
+            const axiosError = error as AxiosError;
+            if (axiosError.response?.status === 400) {
+                const data = axiosError.response.data as { detail: string };
+                setSimpleRes(data.detail || "Erreur lors de la création de la trame Ethernet");
+            } else {
+                setSimpleRes("Erreur inattendue lors de l'appel API");
+            }
         }
-      } else {
-        setSimpleRes("Erreur lors de la simplification de l'adresse IPv6");
-      }
-    }
-  };
+    };
 
-  const [extendIpv6, setExtendIpv6] = useState("");
-  const [extendRes, setExtendRes] = useState("");
 
-  const handleExtend = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    try {
-      const response = await axios.get("/ipv6/extend/" + extendIpv6, {
-        headers: {
-          Authorization: `Bearer ${Cookies.get("access_token")}`,
-        },
-      });
-      const data = response.data;
-      if (response.status === 200) {
-        setExtendRes(data.ipv6);
-      }
-    } catch (error: unknown) {
-      const axiosError = error as AxiosError;
-      if (axiosError.response?.status === 400) {
-        const data = axiosError.response.data as { detail: string };
-        if (data.detail === "Invalid IPv6") {
-          setExtendRes("Adresse IPv6 invalide");
-        } else {
-          setExtendRes("Adresse IPv6 invalide");
-        }
-      } else {
-        setExtendRes("Erreur lors de l'extension de l'adresse IPv6");
-      }
-    }
-  };
+    return (
+        <>
+            <Space space="50px" direction="vertical" margin={{ bottom: "100px" }}>
+                <form onSubmit={handleEthernet}>
+                    <Title level={2}>Rédiger votre trame Ethernet :</Title>
 
-  return (
-    <>
-      <Space space="50px" direction="vertical" margin={{ bottom: "100px" }}>
-        <form onSubmit={handleSimplify}>
-          <Title level={2}>Rédiger votre trame Ethernet :</Title>
-
-          <Input
-            type="text"
-            placeholder="FF:FF:FF:FF:FF:FF"
-            value={simpleIpv6}
-            margin={{ bottom: "20px" }}
-            onChange={(e) => setSimpleIpv6(e.target.value)}
-            required
-            label="Adresse mac destination"
-          />
-          <Input
-            type="text"
-            placeholder="00:11:22:33:44:55"
-            value={simpleIpv6}
-            margin={{ bottom: "20px" }}
-            onChange={(e) => setSimpleIpv6(e.target.value)}
-            required
-            label="Adresse mac source"
-          />
-          <Input
-            type="text"
-            placeholder="0x0800"
-            value={simpleIpv6}
-            margin={{ bottom: "20px" }}
-            onChange={(e) => setSimpleIpv6(e.target.value)}
-            required
-            label="Type de trame Ethernet"
-          />
-          <Button
-            text="Envoyer"
-            primary
-            form="submit"
-            type="input"
-            margin={{ top: "20px" }}
-            disabled={!simpleIpv6}
-            onClick={(e) => {
-              handleSimplify(e);
-            }}
-          />
-          {simpleRes ? (
-            <Box margin={{ top: "20px", bottom: "20px" }}>
-              <Alert
-                severity="success"
-                variant="outlined"
-                style={{ borderRadius: "10px" }}
-              >
-                Résultat : {simpleRes}
-              </Alert>
-            </Box>
-          ) : null}
-        </form>
-
-        <form onSubmit={handleExtend}>
-          <Title level={2}>Réponse : </Title>
-        </form>
-      </Space>
-    </>
-  );
+                    <Input
+                        type="text"
+                        placeholder="FF:FF:FF:FF:FF:FF"
+                        value={masdst}
+                        margin={{ bottom: "20px" }}
+                        onChange={(e) => setMacdst(e.target.value)}
+                        required
+                        label="Adresse mac destination"
+                    />
+                    <Input
+                        type="text"
+                        placeholder="00:11:22:33:44:55"
+                        value={macsrc}
+                        margin={{ bottom: "20px" }}
+                        onChange={(e) => setMacsrc(e.target.value)}
+                        required
+                        label="Adresse mac source"
+                    />
+                    <Input
+                        type="text"
+                        placeholder="0x0800"
+                        value={typetrame}
+                        margin={{ bottom: "20px" }}
+                        onChange={(e) => setTypetrame(e.target.value)}
+                        required
+                        label="Type de trame Ethernet"
+                    />
+                    <Button
+                        text="Envoyer"
+                        primary
+                        form="submit"
+                        type="input"
+                        margin={{ top: "20px" }}
+                        disabled={!masdst || !macsrc || !typetrame}
+                        onClick={(e) => {
+                            handleEthernet(e);
+                        }}
+                    />
+                    {simpleRes ? (
+                        <Box margin={{ top: "20px", bottom: "20px" }}>
+                            <Alert
+                                severity={simpleRes.includes("Erreur") ? "error" : "success"}
+                                variant="outlined"
+                                style={{ borderRadius: "10px" }}
+                            >
+                                Résultat :
+                                <Code
+                                    text={simpleRes}                                    
+                                    language="text"
+                                    theme={solarizedLight}
+                                />
+                            </Alert>
+                        </Box>
+                    ) : null}
+                </form>
+            </Space>
+        </>
+    );
 };
 
 export default EthernetSandbox;
