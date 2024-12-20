d229aae8b59c05cc8da6b74c4e1d56956d6de06b
Eliott-B <eliottb.info@gmail.com>
2024-12-20 08:25:02 +0100

    Fix merge

--- front-js/src/utils/validatePasswd.js
+++ front-js/src/utils/validatePasswd.js
@@ -1,6 +1,6 @@
 export function validate_passwd(passwd) {
     if (!passwd.match(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/)) {
         return false;
-    }p
+    }
     return true;
 }
