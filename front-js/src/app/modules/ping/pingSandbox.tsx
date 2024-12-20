1abcaf056b6b70d193451b50f51daf8b408d4ae0
MaxOuvrard <118462372+MaxOuvrard@users.noreply.github.com>
2024-12-20 10:48:08 +0100

    Add sandbox for ping card module

--- front-js/src/app/modules/ping/pingSandbox.tsx
+++ front-js/src/app/modules/ping/pingSandbox.tsx
@@ -9,34 +9,66 @@ import Cookies from "js-cookie";
 import { AxiosError } from "axios";
 import { Alert } from "@mui/material";
 import Box from "@/components/Box";
+import test from "node:test";
 
 const pingSandbox: React.FC = () => {
-  const [testPing, setTestPing] = useState("");
+  // const [testPing, setTestPing] = useState("");
+  const [command, setCommand] = useState<string>("");
   const [simpleRes, setSimpleRes] = useState("");
+
+  const [testPing, setTestPing] = useState<{
+    rtt_ms: number;
+    packet_size: number;
+    ttl: number;
+    source: string;
+    destination: string;
+  } | null>(null);
+
+  const [error, setError] = useState<string>("");
 
   const handleSimplify = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
+    const parts = command.split(" ");
+    if (parts.length < 2 || parts.length > 2) {
+      setSimpleRes("Commande invalide");
+      return;
+    }
+    if (parts[0] !== "ping") {
+      setSimpleRes("Commande invalide");
+      return;
+    }
     try {
-      const response = await axios.get("/scapy/ip/ " + testPing, {
+      const response = await axios.get("/scapy/ping/" + parts[1], {
         headers: {
           Authorization: `Bearer ${Cookies.get("access_token")}`,
         },
       });
       const data = response.data;
       if (response.status === 200) {
-        setSimpleRes(data.ping);
+        setSimpleRes("Ping réussi");
+        const formattedPing = {
+          rtt_ms: data.rtt_ms,
+          packet_size: data.packet_size,
+          ttl: data.ttl,
+          source: data.source,
+          destination: data.destination,
+        };
+
+        setTestPing(formattedPing);
+        setError("");
       }
     } catch (error: unknown) {
+      setSimpleRes("");
       const axiosError = error as AxiosError;
       if (axiosError.response?.status === 400) {
         const data = axiosError.response.data as { detail: string };
         if (data.detail === "Invalid ping") {
-          setSimpleRes("Ping échoué");
+          setError("Ping échoué");
         } else {
-          setSimpleRes("Ping échoué");
+          setError("Ping échoué");
         }
       } else {
-        setSimpleRes("Erreur lors de la tentative de ping");
+        setError("Erreur lors de la tentative de ping");
       }
     }
   };
@@ -45,29 +77,29 @@ const pingSandbox: React.FC = () => {
       <Space space="50px" direction="vertical" margin={{ bottom: "100px" }}>
         <>
         <form onSubmit={handleSimplify}>
-          <Title level={2}>Simplification d&rsquo;une IPv6</Title>
+          <Title level={2}>Envoi de requête de ping</Title>
 
           <Input
             type="text"
             placeholder="ex : ping 127.0.0.1"
-            value={testPing}
+            value={command}
             margin={{ bottom: "20px" }}
-            onChange={(e) => setTestPing(e.target.value)}
+            onChange={(e) => setCommand(e.target.value)}
             required
             label="Ping"
           />
           <Button
-            text="Simplifier"
+            text="Envoyer le ping"
             primary
             form="submit"
             type="input"
             margin={{ top: "20px" }}
-            disabled={!testPing}
+            disabled={!command}
             onClick={(e) => {
               handleSimplify(e);
             }}
           />
-          {simpleRes ? (
+          {simpleRes && testPing ? (
             <Box margin={{ top: "20px", bottom: "20px" }}>
               <Alert
                 severity="success"
@@ -75,6 +107,42 @@ const pingSandbox: React.FC = () => {
                 style={{ borderRadius: "10px" }}
               >
                 Résultat : {simpleRes}
+                <Space />
+                <table style={{ borderCollapse: "collapse", width: "100%" }}>
+                  <tbody>
+                    <tr style={{ borderBottom: "1px solid black" }}>
+                      <th style={{ paddingRight: "1.5em" }}>Source</th>
+                      <td>{testPing.source}</td>
+                    </tr>
+                    <tr style={{ borderBottom: "1px solid black" }}>
+                      <th style={{ paddingRight: "1.5em" }}>Destination</th>
+                      <td>{testPing.destination}</td>
+                    </tr>
+                    <tr style={{ borderBottom: "1px solid black" }}>
+                      <th style={{ paddingRight: "1.5em" }}>TTL</th>
+                      <td>{testPing.ttl}</td>
+                    </tr>
+                    <tr style={{ borderBottom: "1px solid black" }}>
+                      <th style={{ paddingRight: "1.5em" }}>RTT (ms)</th>
+                      <td>{testPing.rtt_ms}</td>
+                    </tr>
+                    <tr style={{ borderBottom: "1px solid black" }}>
+                      <th style={{ paddingRight: "1.5em" }}>Taille du paquet</th>
+                      <td>{testPing.packet_size}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </Alert>
+            </Box>
+          ) : null}
+          {error? (
+            <Box margin={{ top: "20px", bottom: "20px" }}>
+              <Alert
+                severity="error"
+                variant="outlined"
+                style={{ borderRadius: "10px" }}
+              >
+                {error}
               </Alert>
             </Box>
           ) : null}
