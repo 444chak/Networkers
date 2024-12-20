eefb2f026ae70220991d85c948e3dd4adfa7252f
SaAxok <grv.kylian@gmail.com>
2024-12-20 10:00:48 +0100

    continue ethernet

--- front-js/src/app/modules/ethernet/page.tsx
+++ front-js/src/app/modules/ethernet/page.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 import axios from "@/axiosConfig";
 import { useEffect, useState } from "react";
-import { CircularProgress, Grid2 } from "@mui/material";
+import { CircularProgress, Grid, Grid2, Input } from "@mui/material";
 import Title from "@/components/Title";
 import { EmojiProvider } from "react-apple-emojis";
 import emojiData from "react-apple-emojis/src/data.json";
@@ -95,48 +95,60 @@ export default function Dashboard() {
                 ) : (
                     <>
                         <Box margin={{ top: "50px", bottom: "50px", left: "10%" }}>
-                            <Space space="70px" direction="vertical" >
-
-                                <Space space="15px">
-                                    <Title level={1}>Ethernet</Title>
-                                    <Text>
-                                        Bienvenue sur le module Ethernet. Ce module génère une trame Ethernet basée sur les paramètres fournis.
-                                        Elle utilise les informations sur les adresses MAC source et destination ainsi que le type Ethernet (exprimé en hexadécimal).
-                                        La trame générée est renvoyée au format JSON.
-                                    </Text>
-                                </Space>
-
-                                <Grid2 container spacing={3}>
-                                    <Grid2 size={6}>
-                                        <Space space="15px">
-                                            <Title level={2}>Entrées : </Title>
-                                            <Text>・Adresse MAC de destination, (par exemple, FF:FF:FF:FF:FF:FF).</Text>
-                                            <Text>・Adresse MAC source, (par exemple, 00:11:22:33:44:55).</Text>
-                                            <Text>・Type de trame ethernet, (cf Types de trames).</Text>
-                                        </Space>
-                                    </Grid2>
-                                    <Grid2 size={6}>
-                                        <Space space="15px">
-                                            <Title level={2}>Types de trames Ethernet :</Title>
-                                            <Text>・0x0800 pour IPv4</Text>
-                                            <Text>・0x0806 pour ARP</Text>
-                                            <Text>・0x86DD pour IPv6</Text>
-                                            <Text>・0x8100 pour VLAN</Text>
-                                        </Space>
-                                    </Grid2>
-                                    <Grid2 size={6}>
-                                        <Space space="15px">
-                                            <Title level={2}>Exemple de réponse 200 OK</Title>
-                                            <Image
+                            <Grid2 container spacing={5}>
+                                <Grid2 size={6}>
+                                    <Space space="15px">
+                                        <Title level={1}>Ethernet</Title>
+                                        <Text>
+                                            Bienvenue sur le module Ethernet. Ce module génère une trame Ethernet basée sur les paramètres fournis.
+                                            Elle utilise les informations sur les adresses MAC source et destination ainsi que le type Ethernet (exprimé en hexadécimal).
+                                            La trame générée est renvoyée au format JSON.
+                                        </Text>
+                                    </Space>
+                                </Grid2>
+                                <Grid2 size={6}>
+                                    <Space space="15px">
+                                        <Title level={2}>Entrées : </Title>
+                                        <Text>・Adresse MAC de destination, (par exemple, FF:FF:FF:FF:FF:FF).</Text>
+                                        <Text>・Adresse MAC source, (par exemple, 00:11:22:33:44:55).</Text>
+                                        <Text>・Type de trame ethernet, (cf Types de trames).</Text>
+                                    </Space>
+                                </Grid2>
+                                <Grid2 size={6}>
+                                    <Space space="15px">
+                                        <Title level={2}>Types de trames Ethernet :</Title>
+                                        <Text>・0x0800 pour IPv4</Text>
+                                        <Text>・0x0806 pour ARP</Text>
+                                        <Text>・0x86DD pour IPv6</Text>
+                                        <Text>・0x8100 pour VLAN</Text>
+                                    </Space>
+                                </Grid2>
+                                <Grid2 size={6}>
+                                    <Space space="15px">
+                                        <Title level={2}>Exemple de réponse 200 OK</Title>
+                                        <Image
                                             src="/modules_assets/example_ethernet.png"
                                             alt="Example ethernet"
                                             width={500}
                                             height={200}
-                                            style={{ width: "auto", height: "auto" }}/>
-                                        </Space>
-                                    </Grid2>
+                                            style={{ width: "auto", height: "auto" }} />
+                                    </Space>
                                 </Grid2>
-                            </Space>
+                                <Grid2 size={6}>
+                                    <Space space="15px">
+                                        <Title level={2}>Exemple :</Title>
+                                        <Input
+                                            placeholder="Adresse MAC de destination" fullWidth />
+                                        <Input
+                                            placeholder="Adresse MAC source" fullWidth />
+                                        <Input
+                                            placeholder="Type de trame Ethernet" fullWidth />
+                                    </Space>
+                                </Grid2>
+                                <Grid2 size={6}>
+                                    <Title level={2}>Réponse :</Title>
+                                </Grid2>
+                            </Grid2>
                         </Box>
 
                     </>
