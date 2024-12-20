6d7178320ef372a4c9b9df05df32752b4f6f281b
Eliott-B <eliottb.info@gmail.com>
2024-12-20 16:22:00 +0100

    Don't ask for database

--- set_env.py
+++ set_env.py
@@ -15,6 +15,7 @@ GREEN = "\x1b[32m"
 BLUE = "\x1b[34m"
 RESET = "\x1b[0m"
 
+
 def check_env():
     """Check if the .env directory exists.
     If it doesn't, create it.
@@ -30,12 +31,12 @@ def check_env():
         print("Removing .env/db file...")
         remove(ENV_DB)
 
+
 def create_db_env():
     """Create the .env/db file."""
     print("Database configuration:")
-    with open(ENV_DB, 'w', encoding="UTF-8") as f:
-        database = input("Database: ")
-        f.write("MYSQL_DATABASE=" + database + "\n")
+    with open(ENV_DB, "w", encoding="UTF-8") as f:
+        f.write("MYSQL_DATABASE=networkers\n")
         user = input("Database user: ")
         f.write("MYSQL_USER=" + user + "\n")
         password = input("Database password: ")
@@ -44,23 +45,26 @@ def create_db_env():
         f.write("MYSQL_ROOT_PASSWORD=" + passwordroot + "\n")
         f.close()
 
+
 def create_common_env():
     """Create the .env/common file."""
     print("Common configuration:")
-    with open(ENV_COMMON, 'w', encoding="UTF-8") as f:
+    with open(ENV_COMMON, "w", encoding="UTF-8") as f:
         common_key = input("Common key: ")
         f.write("COMMON_KEY=" + common_key + "\n")
         f.close()
 
+
 def create_api_env():
     """Create the .env/api file."""
     print("API configuration:")
-    with open(ENV_API, 'w', encoding="UTF-8") as f:
+    with open(ENV_API, "w", encoding="UTF-8") as f:
         jwt_secret_key = input("JWT Secret Key: ")
         f.write("JWT_SECRET_KEY=" + jwt_secret_key + "\n")
         jwt_refresh_key = input("JWT Refresh Key: ")
         f.write("JWT_REFRESH_KEY=" + jwt_refresh_key + "\n")
         f.close()
+
 
 if __name__ == "__main__":
     print(f"{BLUE}--------------------")
