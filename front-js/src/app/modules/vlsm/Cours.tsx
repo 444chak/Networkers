92a837c5b21eb76c507632d155a541b9c96b0272
Chak <444chak@gmail.com>
2024-12-20 14:08:46 +0100

    🐞 fix: Ajout d'un indicateur de chargement et gestion des erreurs pour la récupération des adresses IP restantes

--- front-js/src/app/modules/vlsm/Cours.tsx
+++ front-js/src/app/modules/vlsm/Cours.tsx
@@ -4,19 +4,23 @@ import React from "react";
 import Space from "@/components/Space";
 import { Emoji, EmojiProvider } from "react-apple-emojis";
 import emojiData from "react-apple-emojis/src/data.json";
-import { Alert } from "@mui/material";
+import { Alert, CircularProgress } from "@mui/material";
 import axios from "@/axiosConfig";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 import { Code, solarizedLight } from "react-code-blocks";
 import Box from "@/components/Box";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
+import { AxiosError } from "axios";
 
 const Cours: React.FC = () => {
   const [addrRestante, setAddrRestante] = React.useState("");
   const [res, setRes] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const handleAddrRestante = async (e: { preventDefault: () => void }) => {
+    setIsLoading(true);
+    setRes("");
     e.preventDefault();
     try {
       const response = await axios.get("/ipv4/vlsm/192.168.1.0/15,30,50", {
@@ -29,19 +33,26 @@ const Cours: React.FC = () => {
         let lastRange = data.subnet[data.subnet.length - 1].range;
         lastRange = lastRange.split(" - ")[1];
         const listLastRange = lastRange.split(".");
-        const newLastDigit = parseInt(listLastRange[listLastRange.length - 1]) + 1;
+        const newLastDigit =
+          parseInt(listLastRange[listLastRange.length - 1]) + 1;
         listLastRange.pop();
         listLastRange.push(newLastDigit.toString());
-        const ok = listLastRange.join('.') + " - 192.168.1.254";
+        const ok = listLastRange.join(".") + " - 192.168.1.254";
         if (addrRestante == ok) {
           setRes("Bravo !");
-        }
-        else {
-          setRes("");
+        } else {
+          setRes("Mauvaise réponse !");
         }
       }
     } catch (error: unknown) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response?.status === 400) {
+        setRes("Erreur interne");
+      } else {
+        setRes("Erreur interne");
+      }
     }
+    setIsLoading(false);
   };
 
   return (
@@ -52,18 +63,19 @@ const Cours: React.FC = () => {
           <Text>
             VLSM (Variable Length Subnet Masking) est une technique de
             sous-réseautage qui permet de créer des sous-réseaux de différentes
-            tailles. Cette technique est utilisée pour optimiser l&rsquot;utilisation
-            des adresses IP.
+            tailles. Cette technique est utilisée pour optimiser
+            l&rsquo;utilisation des adresses IP.
           </Text>
           <Title level={2} margin={{ top: "2rem" }}>
             Fonctionnement VLSM
           </Title>
           <Text>
             VLSM permet de diviser un réseau en sous-réseaux de différentes
-            tailles. Chaque sous-réseau peut accueillir un nombre minimisé
-            de machine. Cela permet d&rsquot;optimiser l&rsquo;utilisation
-            des adresses IP.
-            <Space />
+            tailles. Chaque sous-réseau peut accueillir un nombre minimisé de
+            machine. Cela permet d&rsquo;optimiser l&rsquo;utilisation des
+            adresses IP.
+          </Text>
+          <Text>
             Attention il ne faut pas oublier de réserver une adresse pour le
             réseau et une pour le broadcast.
           </Text>
@@ -71,8 +83,10 @@ const Cours: React.FC = () => {
             Essayez ! <Emoji name="test-tube" width={32} />
           </Title>
           <Text>
-            Notre réseau (<Code text="192.168.1.0" language="text" theme={solarizedLight} />) contient des sous-réseaux avec 15 machines, 30 machines et 50 machines.
-            Qu&rsquo;elle est la plage d&rsquo;adresses IP restante ?
+            Notre réseau{" "}
+            <Code text="192.168.1.0" language="text" theme={solarizedLight} />{" "}
+            contient des sous-réseaux avec 15 machines, 30 machines et 50
+            machines. Qu&rsquo;elle est la plage d&rsquo;adresses IP restante ?
           </Text>
           <form onSubmit={handleAddrRestante}>
             <Input
@@ -89,21 +103,17 @@ const Cours: React.FC = () => {
           <Box margin={{ top: "20px", bottom: "20px" }}>
             {res ? (
               <Alert
-                severity="success"
+                severity={res === "Bravo !" ? "success" : "error"}
                 variant="outlined"
                 style={{ borderRadius: "10px" }}
               >
-                Bien joué !
+                {res}
               </Alert>
-            ) : (
-              <Alert
-                severity="error"
-                variant="outlined"
-                style={{ borderRadius: "10px" }}
-              >
-                Et non !
-              </Alert>
-            )}
+            ) : isLoading ? (
+              <Box align="center">
+                <CircularProgress />
+              </Box>
+            ) : null}
           </Box>
         </Space>
       </EmojiProvider>
