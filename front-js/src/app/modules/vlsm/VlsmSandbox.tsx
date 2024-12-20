51abf0412d49b398f6c11a915cac23d7583a5474
Chak <444chak@gmail.com>
2024-12-20 14:09:50 +0100

    🐞 fix: Ajout d'un indicateur de chargement et gestion des erreurs pour la récupération des sous-réseaux dans VLSM

--- front-js/src/app/modules/vlsm/VlsmSandbox.tsx
+++ front-js/src/app/modules/vlsm/VlsmSandbox.tsx
@@ -6,8 +6,26 @@ import React from "react";
 import { useState } from "react";
 import axios from "@/axiosConfig";
 import Cookies from "js-cookie";
-import { Alert } from "@mui/material";
+import {
+  Alert,
+  CircularProgress,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import Box from "@/components/Box";
+import { AxiosError } from "axios";
+
+interface Subnet {
+  network: string;
+  mask: string;
+  broadcast: string;
+  range: string;
+}
 
 const VlsmSandbox: React.FC = () => {
   const [baseIp, setBaseIp] = useState("");
@@ -15,18 +33,26 @@ const VlsmSandbox: React.FC = () => {
   const [subnets, setSubnets] = useState<
     Array<{ network: string; mask: string; broadcast: string; range: string }>
   >([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const handleSimplify = async (e: { preventDefault: () => void }) => {
+  const handleSubnets = async (e: { preventDefault: () => void }) => {
+    setIsLoading(true);
+    setError("");
+    setSubnets([]);
     e.preventDefault();
     try {
-      const response = await axios.get("/ipv4/vlsm/" + baseIp + "/" + nombreAddr, {
-        headers: {
-          Authorization: `Bearer ${Cookies.get("access_token")}`,
-        },
-      });
+      const response = await axios.get(
+        "/ipv4/vlsm/" + baseIp + "/" + nombreAddr,
+        {
+          headers: {
+            Authorization: `Bearer ${Cookies.get("access_token")}`,
+          },
+        }
+      );
       const data = response.data;
       if (response.status === 200) {
-        const formattedSubnets = data.subnet.map((subnet: any) => ({
+        const formattedSubnets = data.subnet.map((subnet: Subnet) => ({
           network: subnet.network,
           mask: subnet.mask,
           broadcast: subnet.broadcast,
@@ -35,13 +61,20 @@ const VlsmSandbox: React.FC = () => {
         setSubnets(formattedSubnets);
       }
     } catch (error: unknown) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response?.status === 400) {
+        setError("Erreur interne");
+      } else {
+        setError("Erreur interne inconnue");
+      }
     }
+    setIsLoading(false);
   };
 
   return (
     <>
       <Space space="50px" direction="vertical" margin={{ bottom: "100px" }}>
-        <form onSubmit={handleSimplify}>
+        <form onSubmit={handleSubnets}>
           <Title level={2}>Création d&rsquo;un sous réseau</Title>
 
           <Input
@@ -63,14 +96,14 @@ const VlsmSandbox: React.FC = () => {
             label="Nombre d&rsquo;adresses à attribuer (séparées par des virgules)"
           />
           <Button
-            text="Simplifier"
+            text="Trouver les sous-réseaux"
             primary
             form="submit"
             type="input"
             margin={{ top: "20px" }}
             disabled={!baseIp || !nombreAddr}
             onClick={(e) => {
-              handleSimplify(e);
+              handleSubnets(e);
             }}
           />
           {subnets.length > 0 ? (
@@ -80,27 +113,57 @@ const VlsmSandbox: React.FC = () => {
                 variant="outlined"
                 style={{ borderRadius: "10px" }}
               >
-                Résultat :
-                <table style={{ borderCollapse: "collapse", width: "100%" }}>
-                  <thead>
-                    <tr style={{ borderBottom: "1px solid black" }}>
-                      <th style={{ paddingRight: "1.5em" }}>Network</th>
-                      <th style={{ paddingRight: "1.5em" }}>Mask</th>
-                      <th style={{ paddingRight: "1.5em" }}>Broadcast</th>
-                      <th style={{ paddingRight: "1.5em" }}>Range</th>
-                    </tr>
-                  </thead>
-                  <tbody>
+                Sous-réseaux créés avec succès !
+              </Alert>
+              <TableContainer
+                component={Paper}
+                sx={{ marginTop: "20px", backgroundColor: "#f6f6f666" }}
+              >
+                <Table style={{ borderCollapse: "collapse", width: "100%" }}>
+                  <TableHead>
+                    <TableRow style={{ borderBottom: "1px solid black" }}>
+                      <TableCell style={{ paddingRight: "1.5em" }}>
+                        Network
+                      </TableCell>
+                      <TableCell style={{ paddingRight: "1.5em" }}>
+                        Mask
+                      </TableCell>
+                      <TableCell style={{ paddingRight: "1.5em" }}>
+                        Broadcast
+                      </TableCell>
+                      <TableCell style={{ paddingRight: "1.5em" }}>
+                        Range
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
                     {subnets.map((subnet, index) => (
-                      <tr key={index} style={{ borderBottom: "1px solid black" }}>
-                        <td>{subnet.network}</td>
-                        <td>{subnet.mask}</td>
-                        <td>{subnet.broadcast}</td>
-                        <td>{subnet.range}</td>
-                      </tr>
+                      <TableRow
+                        key={index}
+                        style={{ borderBottom: "1px solid black" }}
+                      >
+                        <TableCell>{subnet.network}</TableCell>
+                        <TableCell>{subnet.mask}</TableCell>
+                        <TableCell>{subnet.broadcast}</TableCell>
+                        <TableCell>{subnet.range}</TableCell>
+                      </TableRow>
                     ))}
-                  </tbody>
-                </table>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Box>
+          ) : isLoading ? (
+            <Box align="center" margin={{ top: "20px", bottom: "20px" }}>
+              <CircularProgress />
+            </Box>
+          ) : error ? (
+            <Box margin={{ top: "20px", bottom: "20px" }}>
+              <Alert
+                severity="error"
+                variant="outlined"
+                style={{ borderRadius: "10px" }}
+              >
+                {error}
               </Alert>
             </Box>
           ) : null}
