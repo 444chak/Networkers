e67bf96665688830f53dd3a11f6e42bf27799cca
Chak <444chak@gmail.com>
2024-12-20 10:29:19 +0100

    ✨feature: Ajouter la conversion d'adresses IPv4 binaire en décimal et la gestion du CIDR

--- front-js/src/app/modules/convert_ipv4/Cours.tsx
+++ front-js/src/app/modules/convert_ipv4/Cours.tsx
@@ -28,6 +28,14 @@ const Cours: React.FC = () => {
   const [resHex2dec, setResHex2dec] = useState("");
   const [validHex2dec, setValidHex2dec] = useState(false);
 
+  const [bin2dec, setBin2dec] = useState("");
+  const [resBin2dec, setResBin2dec] = useState("");
+  const [validBin2dec, setValidBin2dec] = useState(false);
+
+  const [cidr, setCidr] = useState("");
+  const [resCidr, setResCidr] = useState("");
+  const [validCidr, setValidCidr] = useState(false);
+
   const handleConvertToBinary = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     try {
@@ -39,7 +47,9 @@ const Cours: React.FC = () => {
       const data = response.data;
       if (response.status === 200) {
         setResDec2bin(data.ipv4);
-        setValidDec2bin(resDec2bin == dec2bin);
+        setTimeout(() => {
+          setValidDec2bin(data.ipv4 === dec2bin);
+        }, 0);
       }
     } catch (error: unknown) {
       const axiosError = error as AxiosError;
@@ -69,7 +79,9 @@ const Cours: React.FC = () => {
       const data = response.data;
       if (response.status === 200) {
         setResDec2hex(data.ipv4);
-        setValidDec2hex(resDec2hex.toLowerCase() === dec2hex.toLowerCase());
+        setTimeout(() => {
+          setValidDec2hex(data.ipv4.toLowerCase() === dec2hex.toLowerCase());
+        }, 0);
       }
     } catch (error: unknown) {
       const axiosError = error as AxiosError;
@@ -99,7 +111,9 @@ const Cours: React.FC = () => {
       const data = response.data;
       if (response.status === 200) {
         setResHex2dec(data.ipv4);
-        setValidHex2dec(resHex2dec == hex2dec);
+        setTimeout(() => {
+          setValidHex2dec(data.ipv4 === hex2dec);
+        }, 0);
       }
     } catch (error: unknown) {
       const axiosError = error as AxiosError;
@@ -112,6 +126,71 @@ const Cours: React.FC = () => {
         }
       } else {
         setResHex2dec("false");
+      }
+    }
+  };
+
+  const handleConvertBinaryToDecimal = async (e: {
+    preventDefault: () => void;
+  }) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        "/ipv4/bin_to_dec/10101100.00010000.00100110.10000011",
+        {
+          headers: {
+            Authorization: `Bearer ${Cookies.get("access_token")}`,
+          },
+        }
+      );
+      const data = response.data;
+      if (response.status === 200) {
+        setResBin2dec(data.ipv4);
+        setTimeout(() => {
+          setValidBin2dec(data.ipv4 === bin2dec);
+        }, 0);
+      }
+    } catch (error: unknown) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response?.status === 400) {
+        const data = axiosError.response.data as { detail: string };
+        if (data.detail === "Invalid IPv4") {
+          setResBin2dec("false");
+        } else {
+          setResBin2dec("false");
+        }
+      } else {
+        setResBin2dec("false");
+      }
+    }
+  };
+
+  const handleCIDR = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get("/ipv4/cidr/193.51.31.90?mask=24", {
+        headers: {
+          Authorization: `Bearer ${Cookies.get("access_token")}`,
+        },
+      });
+      const data = response.data;
+      if (response.status === 200) {
+        setResCidr(data.ipv4);
+        setTimeout(() => {
+          setValidCidr(data.ipv4 === cidr);
+        }, 0);
+      }
+    } catch (error: unknown) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response?.status === 400) {
+        const data = axiosError.response.data as { detail: string };
+        if (data.detail === "Invalid CIDR") {
+          setResCidr("false");
+        } else {
+          setResCidr("false");
+        }
+      } else {
+        setResCidr("false");
       }
     }
   };
@@ -328,6 +407,141 @@ const Cours: React.FC = () => {
                   style={{ borderRadius: "10px" }}
                 >
                   Conversion échouée !
+                </Alert>
+              ))}
+          </Box>
+          <Title level={2} margin={{ top: "2rem" }}>
+            Conversion d&rsquo;une adresse IPv4 binaire en décimal
+          </Title>
+          <Text>
+            Enfin, vous pouvez également convertir une adresse IPv4 binaire en
+            décimal.
+          </Text>
+          <Title level={2} margin={{ top: "2rem" }}>
+            Essayez ! <Emoji name="test-tube" width={32} />
+          </Title>
+          Convertissez cette adresse IPv4 en décimal
+          <Code
+            text="10101100.00010000.00100110.10000011"
+            language="text"
+            theme={solarizedLight}
+          />
+          <form onSubmit={handleConvertBinaryToDecimal}>
+            <Input
+              onChange={(e) => setBin2dec(e.target.value)}
+              value={bin2dec}
+              type="text"
+              placeholder="Adresse IPv4"
+              margin={{ bottom: "20px" }}
+              required
+              label="Adresse IPv4"
+            />
+            <Button text="Convertir" primary form="submit" type="input" />
+          </form>
+          <Box margin={{ top: "20px", bottom: "20px" }}>
+            {resBin2dec &&
+              (validBin2dec ? (
+                <Alert
+                  severity="success"
+                  variant="outlined"
+                  style={{ borderRadius: "10px" }}
+                >
+                  Conversion réussie !
+                </Alert>
+              ) : (
+                <Alert
+                  severity="error"
+                  variant="outlined"
+                  style={{ borderRadius: "10px" }}
+                >
+                  Conversion échouée !
+                </Alert>
+              ))}
+          </Box>
+          <Title level={2} margin={{ top: "2rem" }}>
+            CIDR
+          </Title>
+          <Text>
+            Le CIDR (Classless Inter-Domain Routing) est une méthode utilisée
+            pour allouer des adresses IP et des plages d&rsquo;adresses IP.
+          </Text>
+          <Text>
+            Le CIDR (Classless Inter-Domain Routing) permet de spécifier
+            précisément la taille d&rsquo;un réseau en ajoutant un suffixe à
+            l&rsquo;adresse IP, indiquant le nombre de bits utilisés pour
+            l&rsquo;identifiant réseau.
+          </Text>
+          <Text>
+            Par exemple,{" "}
+            <Code
+              text="192.168.1.0/24"
+              language="text"
+              theme={solarizedLight}
+            />{" "}
+            signifie que les 24 premiers bits sont utilisés pour identifier le
+            réseau, laissant 8 bits pour les hôtes.
+          </Text>
+          <Text>
+            Pour calculer une adresse IP à partir d&rsquo;une adresse CIDR, vous
+            devez :
+          </Text>
+          <Text>
+            <Space direction="vertical" space="1rem">
+              <Text>
+                1. Identifier le nombre de bits du réseau (après le /)
+              </Text>
+              <Text>2. Convertir l&rsquo;adresse IP en binaire</Text>
+              <Text>
+                3. Appliquer un masque de sous-réseau correspondant au préfixe
+              </Text>
+              <Text>4. Le résultat donne l&rsquo;adresse réseau</Text>
+            </Space>
+          </Text>
+          <Text>
+            Pour notre exemple précédent, l&rsquo;adresse{" "}
+            <Code
+              text="192.168.1.0/24"
+              language="text"
+              theme={solarizedLight}
+            />{" "}
+            correspond à l&rsquo;adresse réseau{" "}
+            <Code text="192.168.1.0/2" language="text" theme={solarizedLight} />
+            .
+          </Text>
+          <Title level={2} margin={{ top: "2rem" }}>
+            Essayez ! <Emoji name="test-tube" width={32} />
+          </Title>
+          <Text>Calculez l&rsquo;adresse réseau pour :</Text>
+          <Code text="193.51.31.90/24" language="text" theme={solarizedLight} />
+          <form onSubmit={handleCIDR}>
+            <Input
+              onChange={(e) => setCidr(e.target.value)}
+              value={cidr}
+              type="text"
+              placeholder="Adresse réseau"
+              margin={{ bottom: "20px" }}
+              required
+              label="Adresse réseau"
+            />
+            <Button text="Calculer" primary form="submit" type="input" />
+          </form>
+          <Box margin={{ top: "20px", bottom: "20px" }}>
+            {resCidr &&
+              (validCidr ? (
+                <Alert
+                  severity="success"
+                  variant="outlined"
+                  style={{ borderRadius: "10px" }}
+                >
+                  Calcul réussi !
+                </Alert>
+              ) : (
+                <Alert
+                  severity="error"
+                  variant="outlined"
+                  style={{ borderRadius: "10px" }}
+                >
+                  Calcul échoué !
                 </Alert>
               ))}
           </Box>
